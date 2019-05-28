bf0fb87ef3d9144b202db72ab29656fdb7968a12
tamnk <tamnk@rikkeisoft.com>
2019-05-28 16:36:36 +0700

    simple chat room

--- dist/index.js
+++ dist/index.js
@@ -27,7 +27,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var PORT = process.env.PORT || 4000;
 
 var app = (0, _express2.default)();
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 // Allow cors
 app.use((0, _cors2.default)());
 
@@ -41,6 +42,32 @@ app.set('views', _path2.default.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/', _routes.WebRouter);
 
-app.listen(PORT, function () {
+//listen on every connection
+io.on('connection', function (socket) {
+  console.log('New user connected');
+  io.sockets.to(socket.id).emit('set_id', { socketId: socket.id });
+  //default username
+  socket.username = "Anonymous";
+
+  //listen on change_username
+  socket.on('change_username', function (data) {
+    socket.username = data.username;
+    console.log('Username:', socket.id, socket.rooms, socket.username);
+  });
+
+  //listen on new_message
+  socket.on('new_message', function (data) {
+    //broadcast the new message
+    console.log('add message:', data);
+    io.sockets.emit('new_message', { message: data.message, username: socket.username, id: socket.id });
+  });
+
+  //listen on typing
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', { username: socket.username });
+  });
+});
+
+http.listen(PORT, function () {
   return console.log('Listening on ' + PORT);
 });
--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ import { WebRouter } from './routes';
 const PORT = process.env.PORT || 4000
 
 const app = express();
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 // Allow cors
 app.use(cors());
 
@@ -21,4 +22,30 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use('/', WebRouter);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+//listen on every connection
+io.on('connection', (socket) => {
+  console.log('New user connected')
+  io.sockets.to(socket.id).emit('set_id', { socketId: socket.id });
+  //default username
+  socket.username = "Anonymous"
+
+  //listen on change_username
+  socket.on('change_username', (data) => {
+    socket.username = data.username
+    console.log('Username:', socket.id, socket.rooms, socket.username);
+  })
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+    //broadcast the new message
+    console.log('add message:', data);
+    io.sockets.emit('new_message', { message: data.message, username: socket.username, id: socket.id });
+  })
+
+  //listen on typing
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', { username: socket.username })
+  })
+})
+
+http.listen(PORT, () => console.log(`Listening on ${PORT}`));
